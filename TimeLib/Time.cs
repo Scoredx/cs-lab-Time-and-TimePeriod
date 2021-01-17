c417640b65708ef6d8f18b0ba1fddfcd137210f9
Scored <multiusage2@o2.pl>
2021-01-17 13:32:16 +0100

    Final

---
+++ TimeLib/Time.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace TimeLib
+{
+    public struct Time : IEquatable<Time>, IComparable<Time>
+    {
+        private byte hour, minute, second;
+        public readonly byte Hour => hour;
+        public  readonly byte Minute => minute;
+        public readonly byte Second => second;
+
+        //var check
+        public Time(byte _hours, byte _minutes = 0, byte _seconds = 0)
+        {
+            if (_hours < 0 || _minutes < 0 || _seconds < 0 || _hours > 23 || _minutes > 59 || _seconds > 59)
+                throw new ArgumentOutOfRangeException("Czas nie jest wprowadzony poprawnie");
+
+            second = _seconds;
+            minute = _minutes;
+            hour = _hours;
+        }
+        //conversion to bytes
+        public Time(string time)
+        {
+            var times = time.Split(':');
+            hour = Convert.ToByte(times[0]);
+            minute = Convert.ToByte(times[1]);
+            second = Convert.ToByte(times[2]);
+
+        }
+
+        //write out
+        public override string ToString()
+        {
+            return $"{hour:00}:{minute:00}:{second:00}";
+        }
+
+        //checkIfEqual
+        public bool Equals(Time other)
+        {
+            return hour == other.hour && minute == other.minute && second == other.second;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Time other && Equals(other);
+        }
+
+        //compareBytes
+        public int CompareTo(Time other)
+        {
+            if (hour - other.hour != 0)
+            {
+                return hour - other.hour;
+            }
+            if (minute - other.minute != 0)
+            {
+                return minute - other.minute;
+            }
+            if (second - other.second != 0)
+            {
+                return second - other.second;
+            }
+            return 0;
+        }
+
+        //logicOperators
+        public static bool operator != (Time t1, Time t2)
+        {
+            return !(t1 == t2);
+        }
+        public static bool operator ==(Time t1, Time t2)
+        {
+            return t1.Equals(t2);
+        }
+        public static bool operator < (Time t1, Time t2)
+        {
+            return t1.CompareTo(t2) < 0;
+
+        }
+        public static bool operator > (Time t1, Time t2)
+        {
+            return t1.CompareTo(t2) > 0;
+
+        }
+        public static bool operator <=(Time t1, Time t2)
+        {
+            return t1.CompareTo(t2) <= 0;
+
+        }
+        public static bool operator >= (Time t1, Time t2)
+        {
+            return t1.CompareTo(t2) >= 0;
+
+        }
+
+        public override int GetHashCode()
+        {
+            return (hour, minute, second).GetHashCode();
+        }
+    }
+}
---
+++ TimePeriod/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using TimeLib;
+
+namespace TimePeriod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Time t1 = new Time(9, 14, 00);
+            Time t2 = new Time(0,14);
+            Time t3 = new Time(4, 14, 0);
+            Time t4 = new Time("4:14:00");
+
+            var varFalse = t1.Equals(t2);
+            var varTrue = t4.Equals(t3);
+            Console.WriteLine("Time1: " + t1);
+            Console.WriteLine("Time2: " + t2);
+            Console.WriteLine("Time3: " + t3);
+            Console.WriteLine("Time4: " + t4);
+            Console.WriteLine("Time1 equals Time2: " + varFalse);
+            Console.WriteLine("Time3 equals Time4: "  + varTrue);
+            Console.WriteLine("Compare Time1 and Time2: " + t1.CompareTo(t2));
+            Console.WriteLine("Compare Time3 and Time4: " + t3.CompareTo(t4));
+        }
+    }
+}
